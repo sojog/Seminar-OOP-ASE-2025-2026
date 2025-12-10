8618922022854ed4722535276473ca8b7ef9ec11
Silviu Ojog <sil.ojog@gmail.com>
2025-12-10 08:20:12 +0200

    Todo - seminar 11

---
+++ Seminar11/Grupa1037/Todo.cpp
@@ -0,0 +1,11 @@
+
+
+Trebuie creată clasa Rectangle care să abstractizeze un dreptunghi dintr-un plan (lungime și lățime).
+
+Clasa deţine următoarele caracteristici:
+ - Lungimea și lățimea dreptunghiului.
+
+Clasa deţine următoarele implementări:
+
+ - operatorul de afisare - se printează dimensiunile dreptunghiului;
+ - area - calculează aria dreptunghiului.
