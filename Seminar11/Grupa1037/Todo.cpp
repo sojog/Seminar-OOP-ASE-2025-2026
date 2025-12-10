de63b8b94fa2e6e5b5904f9c540b8243ef2457b8
Silviu Ojog <sil.ojog@gmail.com>
2025-12-10 09:15:34 +0200

    Mostenire

--- Seminar11/Grupa1037/Todo.cpp
+++ Seminar11/Grupa1037/Todo.cpp
@@ -1,6 +1,6 @@
 
 
-Trebuie creată clasa Rectangle care să abstractizeze un dreptunghi dintr-un plan (lungime și lățime).
+1. Trebuie creată clasa Rectangle care să abstractizeze un dreptunghi dintr-un plan (lungime și lățime).
 
 Clasa deţine următoarele caracteristici:
  - Lungimea și lățimea dreptunghiului.
@@ -10,3 +10,6 @@ Clasa deţine următoarele implementări:
  - operatorul de afisare - se printează dimensiunile dreptunghiului;
  - area - calculează aria dreptunghiului.
  - isSquare - spune dacă un dreptunghi este pătrat
+
+
+2. Creați clasa Square care să moștenească din Rectangle (lățimea și lungimea sunt egale)
